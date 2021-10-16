de006902fce0cba5c90e784d2763854a946d1383
GeoffreyXue <xuegeoff@gmail.com>
2021-10-16 17:37:20 -0500

    calendar wip

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -5,33 +5,37 @@ import Row from "react-bootstrap/Row";
 import { Calendar as Cal, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 
-const localizer = momentLocalizer(moment)
-
 import "./Calendar.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
+const localizer = momentLocalizer(moment)
+
+const myEventsList = [];
+
 function Calendar() {
 
-  return (
-    <Container>
-        <Row>
-            <h1 className='m-3'>
-                Calendar
-                <hr/>
-            </h1>
-            
-        </Row>
-        <Row className="m-3">
-            <Cal
-            localizer={localizer}
-            events={myEventsList}
-            startAccessor="start"
-            endAccessor="end"
-            style={{ height: 500 }}
-            />
-        </Row>
-    </Container>
-  );
+    
+
+    return (
+        <Container>
+            <Row>
+                <h1 className='m-3'>
+                    Calendar
+                    <hr/>
+                </h1>
+                
+            </Row>
+            <Row className="m-3">
+                <Cal
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+                />
+            </Row>
+        </Container>
+    );
 }
 
 export default Calendar;
