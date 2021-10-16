2a686f89fca19417646be711a923de11aef4ffc7
Elon <elonmusk@gmail.com>
2021-10-16 18:34:17 -0500

    s

--- backend/app.py
+++ backend/app.py
@@ -91,7 +91,7 @@ def page():
         return jsonify(success = False)
     pages = request.json.get('pages')
     lists = sessions.query(PersonalInfo).filter(PersonalInfo.ID > 3*(pages-1), PersonalInfo.ID <= (pages) * 3  )
-    return Response(json.dumps([i.__dict__ for i in lists]), mimetype= 'application/json')
+    return Response([i.__dict__ for i in lists], mimetype= 'application/data')
 
 @app.route('/')
 def home():
